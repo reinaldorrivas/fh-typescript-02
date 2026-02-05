8d3d9c986610834e19c6cb35979cb58998684f26
René Rivas <reinaldorrivas@gmail.com>
2026-02-04 23:52:29 -0400

    fh-ts-guide: Minor changes

--- src/funciones/functions.ts
+++ src/funciones/functions.ts
@@ -11,7 +11,7 @@ export const functionsTs = (): void => {
 
   console.log("activateBatiSignal:", activateBatiSignal());
 
-  console.log("Tipado de una función: ", typeof activateBatiSignal);
+  console.log("Tipado de una función:", typeof activateBatiSignal);
 
   const heroName = returnName(hero);
 
--- src/funciones/restArgs.ts
+++ src/funciones/restArgs.ts
@@ -5,4 +5,6 @@ export const restArgs = (): void => {
   const superman = fullName("Clark", "Joseph", "Ken");
 
   console.log({ superman });
+
+  console.log("--- END REST ARGUMENTS FILE ---");
 };
