37f71e92abe1e897a180b32917e7de28cd74afb1
René Rivas <reinaldorrivas@gmail.com>
2026-02-04 22:39:33 -0400

    fh-ts-guide: Rest arguments introduction

---
+++ src/funciones/restArgs.ts
@@ -0,0 +1,8 @@
+const fullName = (firstName: string, ...props: string[]): string =>
+  `${firstName} ${props.join(" ")}`;
+
+export const restArgs = (): void => {
+  const superman = fullName("Clark", "Joseph", "Ken");
+
+  console.log({ superman });
+};
